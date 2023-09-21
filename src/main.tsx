756e1c80716f4160a48541eaf902778d1e37f439
s-bogelund <simbj94@gmail.com>
2023-09-21 14:59:52 +0200

    Installed and setup react router

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,8 @@ import App from './App.tsx';
 import './index.css';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n.ts';
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Button } from './components/ui/button.tsx';
 // await i18next.init({
 // 	lng: 'en', // if you're using a language detector, do not define the lng option
 // 	debug: true,
@@ -29,12 +30,27 @@ const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 root.render(
 	<React.StrictMode>
-		<I18nextProvider i18n={i18n}>
-			<ApolloProvider client={client}>
-				<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-					<App />
-				</ThemeProvider>
-			</ApolloProvider>
-		</I18nextProvider>
+		<BrowserRouter>
+			<I18nextProvider i18n={i18n}>
+				<ApolloProvider client={client}>
+					<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+						<Routes>
+							<Route path="/" element={<App />} />
+							<Route
+								path="*"
+								element={
+									<div className="flex flex-col min-w-full min-h-[100vh] justify-center items-center text-4xl gap-8">
+										Something's wrong here mate...
+										<Button>
+											<Link to="/">Go back</Link>
+										</Button>
+									</div>
+								}
+							/>
+						</Routes>
+					</ThemeProvider>
+				</ApolloProvider>
+			</I18nextProvider>
+		</BrowserRouter>
 	</React.StrictMode>
 );
