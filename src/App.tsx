80d0077d0029a5558705c8df1fb1be71575cda8d
s-bogelund <simbj94@gmail.com>
2023-09-21 13:51:05 +0200

    Added and setup apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { ThemeProvider } from '@/components/theme-provider';
 import { Button } from './components/ui/button';
 
 function App() {
 	return (
-		<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-			<div
-				className="flex container flex-col justify-center items-center min-h-[100vh] gap-2
+		<div
+			className="flex container flex-col justify-center items-center min-h-[100vh] gap-2
       "
-			>
-				<h1>Hello World</h1>
-				<Button>Default Shadcn button</Button>
-			</div>
-		</ThemeProvider>
+		>
+			<h1>Hello World</h1>
+			<Button>Default Shadcn button</Button>
+		</div>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@/components/theme-provider';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import App from './App.tsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const client = new ApolloClient({
+	// TODO: Change below to the real API
+	uri: 'https://flyby-router-demo.herokuapp.com/',
+	cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
+	<React.StrictMode>
+		<ApolloProvider client={client}>
+			<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+				<App />
+			</ThemeProvider>
+		</ApolloProvider>
+	</React.StrictMode>
+);
