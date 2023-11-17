de7fb522bc7c7e3e5459e535e21ee6c43ed28658
Mads <Mads@LAPTOP-6FUP8OA4>
2023-11-17 14:19:48 +0100

    Added difficulty and target age

--- src/components/user-dashboard/DashboardReaderStorySummary.tsx
+++ src/components/user-dashboard/DashboardReaderStorySummary.tsx
@@ -1,15 +1,53 @@
 import { Playthrough } from '@/pages/UserDashboardPage'
 import React, { FC } from 'react'
 import { Card } from '../shadcn/ui/card'
+import StoryInfoElement from '../story-homepage/StoryInfoElement'
+import { Icons } from '../icons/Icons'
+import getAgeIcon from '../icons/age-icons'
 
 type DashboardReaderStorySummaryProps = {
     playthrough?: Playthrough,
 }
 
 const DashboardReaderStorySummary: FC<DashboardReaderStorySummaryProps> = ({ playthrough }) => {
+  const renderDifficultyIcons = () => {
+    if (playthrough?.story.difficulty) {
+      const numberOfRenders = parseInt(playthrough?.story.difficulty)
+      const iconArray = Array(numberOfRenders).fill(<></>).map((_, index) => (
+        <StoryInfoElement icon={Icons.Skull} />
+      ));
+      return iconArray;
+    } else {
+      return <p>Ingen sværhedsgrad</p>
+    }
+  }
+  
   return (
-    <Card className='flex border-2'>
-      <Card className='w-full h-fit'></Card>
+    <Card className='flex flex-col border-2 items-center'>
+      <Card className='h-fit text-2xl mb-3'>
+        {playthrough?.story.title}
+      </Card>
+      <Card className='flex flex-row gap-12 text-lg'>
+        <Card className='flex flex-row gap-5'>
+          Sværhedsgrad:
+          <Card className='flex'>{renderDifficultyIcons()}</Card>
+        </Card>
+        <Card className='flex flex-row gap-5'>
+          Alder: 
+          <StoryInfoElement
+            icon={getAgeIcon(playthrough?.story.targetAge ? playthrough?.story.targetAge : 4)}
+            description={`Recommended age: ${playthrough?.story.targetAge || 6}+`}
+            isAgeIcon
+            className="justify-center"
+          />
+        </Card>
+      </Card>
+      <Card>
+        story node title + encounter type
+      </Card>
+      <Card>
+        story node textarea
+      </Card>
     </Card>
   )
 }
